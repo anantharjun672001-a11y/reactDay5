ef3dff2edbcc1663664edc1b21c1a595ce6e1772
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-12-05 09:49:54 +0530

    Base completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Forms from './Components/Forms';
+
+const App = () => {
+  return (
+    <div>
+      <h1>REACT DAY5</h1>
+      <Forms/>
+    </div>
+  );
+};
+
+export default App;

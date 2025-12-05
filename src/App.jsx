0ea1288ff11d587a9397552fde62aadee4e95be4
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-12-05 19:07:12 +0530

    base completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Forms from './Components/Forms';
+import Memocallback from './Components/Memocallback';
 
 const App = () => {
   return (
     <div>
       <h1>REACT DAY5</h1>
       <Forms/>
+      <Memocallback/>
     </div>
   );
 };
---
+++ src/Components/Memocallback.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const Memocallback = () => {
+    const[count,setCount]=useState(0)
+    const[value,setValue]=useState([
+        "men",
+        "women",
+        "Kids",
+        "electronics",
+        "fashion"
+    ])
+    const[search,setSearch]=useState("");
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Memocallback;
